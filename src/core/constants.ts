9fa69dc0fd7452d84d6fca0df3fbabd7e189bb45
Niok-nk <0499niok@gmail.com>
2026-07-16 18:42:08 -0500

    Etapa 1.1-1.2: TimePosition + constantes del sistema Eras

---
+++ src/core/constants.ts
@@ -0,0 +1,3 @@
+export const SEMANAS_POR_PERIODO = 12
+export const PERIODOS_POR_ERA = 52
+export const SEMANAS_POR_ERA = 624
---
+++ src/core/types.ts
@@ -0,0 +1,5 @@
+export interface TimePosition {
+  era: number
+  periodo: number
+  semana: number
+}
