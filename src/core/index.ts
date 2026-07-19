14d73ca9b4b71275ab05bcd184c7e58d99d856d5
Niok-nk <0499niok@gmail.com>
2026-07-19 15:11:48 -0500

    Etapa 1.5: barrel index.ts para core/

---
+++ src/core/index.ts
@@ -0,0 +1,3 @@
+export type { TimePosition } from './types'
+export { semanasTotalesAPosicion, posicionASemanasTotales } from './convert'
+export { SEMANAS_POR_PERIODO, PERIODOS_POR_ERA, SEMANAS_POR_ERA } from './constants'
